e0fae7b6902e05c2ee8687ccc245df5cfc2ea4fa
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-22 11:47:49 +0800

    more functions and conditions

--- project2/js_demo1/script.js
+++ project2/js_demo1/script.js
@@ -20,8 +20,71 @@ const totalFriends = friendShipAge.length
 const averageAge = sum/totalFriends
 console.log("Average friend age",averageAge)
 
+
+/*
+    Some simple functions
+*/
+
+let age = 35
+let sumz =200
+let totals = 5
+
+function makeConsoleLog(){
+    console.log("Hello from console log "+ totals)
+}
+
+function addsToMyAge(incrementBy,multiplyBy){
+    my_Age = (incrementBy *multiplyBy)
+    return my_Age
+}
+new_age = addsToMyAge(5,8)
+console.log("The new age "+new_age)
+
+
+function conditionalFunction(agesParameter){
+    if (agesParameter > 18){
+        return true
+    }
+    else {
+        return false
+    }   
+}
+
+function shouldIgetALicense(my_age,bribe){
+    if(age<=18 && bribe >100 ){
+        console.log('You Pass')
+        return 
+    }
+    
+    if(my_age > 18){
+        console.log('This person is above 18')
+        return
+    }
+    else{
+        if(bribe >100){
+            console.log('You passed')
+        }else{
+            console.log("This guy is below 18")
+        }
+    }
+}
+
+shouldIgetALicense(18,200)
+
+function conditionalFunction(agesParameter){
+    if (agesParameter > 18){
+        return true
+    }
+    else {
+        return false
+    }   
+}
+
+console.log("The age limit+"+conditionalFunction(10))
+
 const timer = document.querySelector('span')
 console.log(timer)
+makeConsoleLog()
 
 setInterval(()=>{
     timer.innerText = new Date().toLocaleDateString()
