416b3d09cc7e5cd13d17f23a0e3161c24107b6b7
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-23 18:51:16 +0800

    half done

--- project_demo/my-app/src/App.tsx
+++ project_demo/my-app/src/App.tsx
@@ -1,51 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from "./components/InputField"
+import {Todo} from "./model"
 
-let name:string
-let age :number | string
-let isStudent:boolean
-let hobbies:number[]
-let role:[number,string]
+const App:React.FC =() => {
+  const [todo,setTodo] = useState<string>("")
+  const[todos,setTodos] = useState<Todo[]>([])
+  
+  const handleAdd = (e:React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos])
+    }
+  }
 
-/*
-  Function Types
-*/
-let printName:(name:string)=>string
+  console.log(todo)
 
-/*
-type Person = {
-  name:string,
-  age?:number
-}
-
-let person:Person = {
-  name:"Mluleki",
-  age:35
-}
-
-let lotsofPeople:Person[];
-*/
-
-interface Personal {
-    name:string;
-    age?:number
-}
-
-type X = Personal & {
-  a:string,
-  b:number
-}
-
-
-interface Guy extends Personal  {
-  profession:string
-}
-
-
-function App() {
   return (
     <div className="App">
-      Hello World
+      <span className = "heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo}  handleAdd = {handleAdd}/>
     </div>
   );
 }
