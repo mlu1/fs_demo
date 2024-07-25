8c779fa59a03dad6d3bb20c187ac479bc5a0da54
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-25 21:10:24 +0800

    blog-posts updated bug fixes

--- project2/blog-posts/src/index.js
+++ project2/blog-posts/src/index.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SingleComment from './SingleComment';
+import profile2 from './images/photo2.jpg'
+import profile3 from './images/photo3.jpg'
+import UserCard from './UserCard';
 
 const App =()=>{
     return(
-           <div className='ui comment'>
-                <div className='comment'>  
-                    <a href = "/" className='avater'>
-                        <img alt ='profile picture'/>
-                    </a>
-                    <div className='content'>
-                        <a href = "/" className='author'>
-                            Sarah
-                        </a>
-                        <div className='metadata'>
-                            <span className='date'>
-                                Today at 5:00pm
-                            </span>
-                        </div>
-                        <div className='text'>
-                                its amazing
-                        </div>
-                    </div>
-                </div>
-            </div> 
+           <div className='ui comments'>
+                <UserCard>
+                        <div>Hello my name is Sarah</div>
+                </UserCard>
+                <UserCard>
+                    <SingleComment name={'Sarah'} 
+                    date={'Today at 5:00pm'}
+                    text = {'Cools'}
+                    picture = {profile3} />
+                </UserCard>
+                <UserCard>
+                <SingleComment name ={'Alex'}
+                    date={'Today at 7:00pm'}
+                    text = {'Not Cools'}
+                    picture = {profile2} />
+                </UserCard>
+                <UserCard>
+                <SingleComment name={'David'}
+                    date={'Today at 8:00pm'}
+                    text = {'Thanks'}
+                    picture = {profile3}
+                    />   
+                 </UserCard>
+                         
+            </div>      
     )
 }
 
