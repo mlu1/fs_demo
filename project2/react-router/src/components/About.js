3924572df9569432903ea5f2e87ef26a269f32a0
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-27 23:43:42 +0800

    routing mini project

---
+++ project2/react-router/src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <div className="ui raised very padded text container segment" style ={{marginTop:'80px'}}>
+                <h3 className='ui header'>About</h3>
+                <p>Some text related to About</p>
+        </div>
+    )
+}
+export default About;
---
+++ project2/react-router/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () =>{
+    return(
+        <div className="ui raised very padded text container segment" style ={{marginTop:'80px'}}>
+                <h3 className='ui header'>Contact</h3>
+                <p>Some text related to contact page stuff</p>
+        </div>
+    )
+}
+export default Contact;
