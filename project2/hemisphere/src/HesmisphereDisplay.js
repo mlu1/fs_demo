193d85e10301123436e634215fb1f8f34c26c143
mlu@1 <mthandemeluleki@yahoo.com>
2024-07-27 10:46:43 +0800

    finalise mini project 3

--- project2/hemisphere/src/HesmisphereDisplay.js
+++ project2/hemisphere/src/HesmisphereDisplay.js
@@ -1,11 +1,38 @@
 import React from "react";
+import northernPic from  './images/northern.jpg'
+import southernPic from  './images/southern.jpg'
+import './hemisphere.css'
 
+const hemisphereConfig = {
+    Northern:{
+        text:'it is Nothern hemisphere',
+        picture:northernPic
 
-const HemisphereDisplay = ()=>{
-    return (
-        <div>Hey we are in</div>
-    )
+    },
+    Southern:{
+        text: 'it is southern hemisphere',
+        picture:southernPic
+    }
+
 }
 
+const HemisphereDisplay = ({latitude})=>{
+
+    const hemisphere = latitude > 0 ? 'Northern' :'Southern';
+    const {text,picture} = hemisphereConfig[hemisphere]
+
+    return (
+        <div className = {hemisphere}>
+            <div className='ui raised text container segment'>
+                <div className="image">
+                    <img src = {picture} alt="hemisphere picture"/>
+                </div>
+                <div className= 'ui teal bottom attached label'>
+                    <h1>{text}</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default HemisphereDisplay
--- project2/hemisphere/src/index.js
+++ project2/hemisphere/src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HemisphereDisplay from "./HesmisphereDisplay";
 
-
 class App extends React.Component{
-        constructor(props){
-            super(props)
-            this.state = {lattitude:null,errorMessage:''}
+        state = {lattitude:null,errorMessage:'' }
+
+        componentDidMount(){
             window.navigator.geolocation.getCurrentPosition(
                 (position) => 
                   this.setState({lattitude:position.coords.latitude})  ,
@@ -14,23 +13,16 @@ class App extends React.Component{
                     this.setState({errorMessage:error.message})
                 }
             );
-        }
-        componentDidMount(){
-            console.log(`componentDidMount`)
 
         }
-
-        componentDidUpdate(){
-            console.log(`componentDidUpdate`)
-        }
-
+        
         render(){
             if (this.state.errorMessage && !this.state.lattitude){
                 return(<div>{this.state.errorMessage}</div>)
             }
             
              if(!this.state.errorMessage && this.state.lattitude){
-                return (<div>{this.state.lattitude}</div>)
+                return (<div><HemisphereDisplay latitude = {this.state.lattitude}/></div>)
              }
              else{
                 return <div>Loading...</div>
