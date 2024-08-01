052c8299e67b0f3d728a9aa3f76a8b1678a59705
mlu@1 <mthandemeluleki@yahoo.com>
2024-08-01 23:58:09 +0800

    star rating app mini-project

--- project3/react-select-menu/src/App.js
+++ project3/react-select-menu/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
-
+import StarRating from './components/star-rating'
 function App() {
   return (
     <div>
      {/* <Accordian/>*/}
-     <RandomColor/> 
+     {/*<RandomColor/> */}
+     <StarRating noOfStars = {10}/>
     </div>
   );
 }
