586b60dedbb1eef5992c0dd0baad128a5b9aec44
mlu@1 <mthandemeluleki@yahoo.com>
2024-08-13 19:07:49 +0800

    create a component

--- project4/estore/src/App.js
+++ project4/estore/src/App.js
@@ -1,52 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-    <header className = "header">
-      <div className = "container-filled">
-        <div className= "row"> 
-            <div className='col-xs-3 col-lg-2'> 
-              <div className="header__logo">
-                <font>estore</font>
-              </div>
-            </div>
-            <div className="col-xl-6 col-lg-7">
-              <div className="input-group">
-                <div className = "input-group-prepend">
-                    <select className ="form-control btn btn-success dropdown-toggle">
-                      <option>ALL</option>
-                      <option>MEN</option>
-                      <option>Woman</option>
-                      <option>Kids</option>
-                    </select>
-                </div>  
-                <input type="text" className="form-control"/> 
-                <div className="input-group-append">
-                    <button className="btn btn-success"> 
-                      <i className="fa fa-search"></i>
-                    </button>
-                </div>
-              </div>
-              <div className="col-xl-6 col -lg-7">
-                  <div className="header__right">
-                    <div className="auth">
-                      <a href="a">Sign in</a>
-                      <a href="a">Sign Up</a>
-                    </div>
-                    <div>
-                      <ul className ="widget"> 
-                        <li><i className="fa fa-heart"></i></li>
-                        <li><i className="fa fa-shopping-cart"></i></li>
-                      </ul>
-                    </div>
-                  </div>
-              </div>
-            </div>
-          </div>
-      </div>
-    </header>
+    <Header/>
   );
 }
-
 export default App;
