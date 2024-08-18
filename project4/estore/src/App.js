5f1bcd80d832aae232ae22bdeacff2b56b2dd029
mlu@1 <mthandemeluleki@yahoo.com>
2024-08-18 18:24:22 +0800

    add transition for hover

--- project4/estore/src/App.js
+++ project4/estore/src/App.js
@@ -9,7 +9,6 @@ function App() {
       <Header/>
       {/*<TopMenu/>}*/}
       <MainContainer/>
-      
     </div>
   );
 }
--- project4/estore/src/component/Product/Product.js
+++ project4/estore/src/component/Product/Product.js
@@ -1,40 +1,36 @@
 import React from "react";
 import './_product.scss'
-import image1 from '../../assets/img/shop/shop-1.jpg';
-
-const products = [
-    require('../../assets/img/shop/shop-1.jpg'),
-    require('../../assets/img/shop/shop-2.jpg'),
-    require('../../assets/img/shop/shop-3.jpg'),
-    require('../../assets/img/shop/shop-4.jpg'),
-    require('../../assets/img/shop/shop-4.jpg'),
-]
 
 const Product=()=>(
-    <div className="row">{
-        products.map((item,index)=>(
-            
-        <div className="col-lg-3 col-md-6">
-            <div className="product__items">
-                <div className="product__item__pic">
-                        <img className ="product__item__pic" src = {item} alt=""/>
-                        <ul className = "product-hover">
-                            <li>
-                                <a href = {item}>
-                                    <span className="fa fa-arrows-alt"/>
-                                </a>
-                            </li>
-                        </ul> 
+    <div className="row">
+       <div className=" col-lg-3 col-md-6">
+            <div className="product__item">
+            <div className="product__item__pic">   
+                <img clasName ="product__item__pic" src = {require("../../assets/img/shop/shop-1.jpg")} alt=""/>
+                <ul className="product__hover">
+                    <li>
+                        <a href={require("../../assets/img/shop/shop-1.jpg")}>
+                            <span className="fa fa-arrows-alt"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="product__item__text">
+                <h6>
+                    <a href="#"> Furry Hood</a>
+                </h6>
+                <div className="rating">
+                    <i className="fa fa-star"/>
+                    <i className="fa fa-star"/>
+                    <i className="fa fa-star"/>
+                    <i className="fa fa-star"/>
+                    <i className="fa fa-star"/>
                 </div>
-                <div className="product__item__text">
-                    <h6> Furry Hooded</h6>
-                </div>
+                <div className="product_price">$600</div>
             </div>
         </div>
-        
-        ))}
-    </div>
-    
+       </div>
+    </div> 
 )
 
 export default Product
