4b9d1be23a4c66bc8946826c35689c692d038a95
mlu1 <mthandemeluleki@yahoo.com>
2024-08-30 10:18:53 +0800

    add nested routers

--- trpc-from-youtube/server/api.ts
+++ trpc-from-youtube/server/api.ts
@@ -1,26 +1,12 @@
 import express from "express"
 import cors from "cors"
-import {initTRPC} from "@trpc/server"
+import { createExpressMiddleware } from "@trpc/server/adapters/express"
+import {appRouter} from "./routers/index"
 
-const t = initTRPC.create()
-
-/*
-    NOTES
-    -procedures that allows us to interact with the server
-    -
-*/
-
-t.router({
-    sayHi:t.procedure.query(()=>{
-        return  "Hi"
-    }),
-    logToServer:t.procedure.input(v =>{
-        if(typeof v === "string")return v
-    })
-})
-
+//app specific interface
 const app = express()
-app.use(cors({origin:"http:/localhost:5173"}))
-app.listen(3000)
+app.use(cors({origin:"http://localhost:5175"}))
+app.use("/trpc",createExpressMiddleware({router:appRouter}))
 
-console.log("Hi")
+app.listen(3000)
+export type AppRouter = typeof appRouter
---
+++ trpc-from-youtube/server/routers/index.ts
@@ -0,0 +1,22 @@
+import {t} from "../trpc"
+import {userRouter} from "./users"
+/*
+    NOTES
+    -procedures that allows us to interact with the server
+    -
+*/
+
+export const appRouter = t.router({
+    sayHi:t.procedure.query(()=>{
+        return  "Hi";
+    }),
+    logToServer:t.procedure.input(v =>{
+        if(typeof v === "string")return v
+        throw new Error("Invalid Input:Expected string")
+    }).mutation(req => {
+        console.log(`Client Says: ${req.input}`)
+        return true;
+    }),
+    users:userRouter,
+})
+
