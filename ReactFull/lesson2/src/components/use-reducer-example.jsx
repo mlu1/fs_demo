6432b64cab46c11d4ab6263645bf0ed8b7b070ab
mlu1 <mthandemeluleki@yahoo.com>
2024-09-24 07:23:28 +0800

    useReducer

--- ReactFull/lesson2/src/components/use-reducer-example.jsx
+++ ReactFull/lesson2/src/components/use-reducer-example.jsx
@@ -4,11 +4,26 @@ const initialState = {
     changeTextStyleFlag:false
 }
 
+const HIDE_TEXT = 'HIDE_TEXT';
+const SHOW_TEXT = 'SHOW_TEXT';
+const CHANGE_TEXT_STYLE = 'CHANGE_TEXT_STYLE';
+
 function reducer(state,action){
     switch(action.type){
-        case value:
-            break;
-        
+        case 'HIDE_TEXT':
+            return {
+                ...state,
+                showTextFlag:false
+            }
+        case 'SHOW_TEXT':
+            return {
+                ...state,showTextFlag:true
+            }
+        case 'CHANGE_TEXT_STYLE':
+                return{
+                    ...state,
+                    changeTextStyleFlag:!state.changeTextStyleFlag
+                }
         default:
             return state
     }
@@ -18,11 +33,12 @@ export default function useReducerExample(){
         const [state,dispatch] = useReducer(reducer,initialState)
         console.log(state);
         return (
+           
             <div>
-                <h1>Use Reducer Example</h1>
-                <button>Hide  Text</button>
-                <button>Show Text</button>
-                <button>Chnage Style</button>
+                {state?.showTextFlag?<h1 style= {{backgroundColor:state?.changeTextStyleFlag ? "black":"red"}}>Use Reducer Hook Example</h1>:null}
+                <button onClick={()=>dispatch({type:HIDE_TEXT})}>Hide  Text</button>
+                <button onClick={()=>dispatch({type:SHOW_TEXT})} >Show Text</button>
+                <button onClick={()=>dispatch({type:CHANGE_TEXT_STYLE})} >Toggle Text Styles</button>
             </div>
         )
 }
