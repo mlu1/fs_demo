ea9f1c32ba429d4329294122ab091d6230665f47
mlu1 <mthandemeluleki@yahoo.com>
2024-09-30 10:57:26 +0800

    complicated form data stuff

--- ReactFull/lesson2/src/components/common-form/index.jsx
+++ ReactFull/lesson2/src/components/common-form/index.jsx
@@ -1,8 +1,36 @@
 import CommonInput from "../common-input"
-function CommonForm({formControls = [],formData,setFormData}){
+function CommonForm({formControls = []}){
+    
+    const formTypes = {
+        INPUT :'input',
+        SELECT :'select',
+        TEXTAREA:'textarea'
+    }
+
+
+    /* render the components */
+    function renderFormElement(getCurrentElement){
+        let content =null;  
+        switch (getCurrentElement?.componentType) {
+            case formTypes.INPUT:
+                content = <CommonInput
+                        label={getCurrentElement.label} 
+                        name = {getCurrentElement.name} 
+                        id = {getCurrentElement.id}
+                        placeholder={getCurrentElement.placeholder}
+                        value={formData[getCurrentElement.name]}/>
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <form>
-            <CommonInput/>
+            {
+                formControls?.length ? formControls.map(singleFormElement =>
+                    renderFormElement(singleFormElement)):null
+            }
         </form>
     )
 }
--- ReactFull/lesson2/src/components/login/index.jsx
+++ ReactFull/lesson2/src/components/login/index.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { loginFormElements } from "../../config";
 import CommonForm from "../common-form";
 
 function LoginComponent(){
+    const [loginFormData,setLoginFormData] = useState({
+        email:'',
+        password:''
+        
+        })
     return(
         <div>
             <h1>Login page/Component</h1>
-            <CommonForm formControls={loginFormElements}/>
+            <CommonForm 
+            formData = {loginFormData}
+            setFormData={(setLoginFormData)}
+            formControls = {loginFormElements}/>
         </div>
     )
 }
