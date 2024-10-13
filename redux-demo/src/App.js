0e5dec4f5f6caa14a9afc171ea75ac1032539869
mlu1 <mthandemeluleki@yahoo.com>
2024-10-14 07:25:38 +0800

    fix app bug

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -3,10 +3,6 @@ import Purchase from './components/Purchase';
 import Cart from './components/Cart';
 import Total from './components/Total';
 
-/*
-  The Provider component makes the Redux store available to any nested components
-  that need access to the Redux store.
-*/
 
 function App() {
   return (
