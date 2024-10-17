26480a1d5a40716f208ca8ce4944bb5ebc958575
mlu1 <mthandemeluleki@yahoo.com>
2024-10-17 09:06:24 +0800

    add children nodes coomponent

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
 import NameTag from './components/NameTag';
 import * as PropTypes from 'prop-types'
+import Box from './components/Box'
 
 
 function App() {
   return (
     <div className="App">
      <NameTag name = {"Mluleki"}/>
+      <Box>
+        just a string.
+        <p>Some html that is not nested</p>
+        <Box>
+          <h2> Another React Component with one child</h2>
+        </Box>
+        <Box>
+          <h2> A nested React component with two children</h2>
+          <p>The second child</p>
+        </Box>
+      </Box>
     </div>
   );
 }
