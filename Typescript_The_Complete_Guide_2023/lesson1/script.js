bc9662012bdc6637247e4e530f7094597418a238
mlu1 <mthandemeluleki@yahoo.com>
2024-10-24 09:58:11 +0800

    arrays lesson

--- Typescript_The_Complete_Guide_2023/lesson1/script.js
+++ Typescript_The_Complete_Guide_2023/lesson1/script.js
@@ -12,10 +12,40 @@ emp = {
         city: 'New Dehli',
         area: 'MBA'
     },
-    display() {
-        console.log(this.empName);
-    },
 };
 let names = [1, 2, 3, 4, 5];
+let surnames = [5, 4, 3, 2, 1];
 names.push(7);
-console.log(names);
+let upDatedNumbers = names.filter((n) => n > 3);
+let upDatedNumbers2 = names.map((n) => n * 3);
+for (let i = 0; i < 4; i++) {
+    console.log(names[i]);
+}
+names.forEach(n => {
+    console.log(n);
+});
+console.log(upDatedNumbers);
+console.log(upDatedNumbers2);
+let listOfEmp = [
+    {
+        eName: "Mlu",
+        eSalary: 1200
+    },
+    {
+        eName: "Nozi",
+        eSalary: 2000
+    }, {
+        eName: "Phumis",
+        eSalary: 700
+    }
+];
+console.log(listOfEmp);
+let listOfNames = listOfEmp.map(ob => ob.eName);
+let listOfNamesUpdated1 = listOfEmp.filter(ob => ob.eSalary > 1000);
+console.log(listOfNames);
+console.log(listOfNamesUpdated1);
+/**ARRAY DESTRUCTURING */
+let [n1, n2] = [1, 2];
+console.log(n1);
+//Array with constructor
+let arr = new Array(1, 2);
