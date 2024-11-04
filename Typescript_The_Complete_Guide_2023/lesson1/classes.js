32a8a7ef5c63cf8d1cff784d9b6e09b35d3ae15e
mlu1 <mthandemeluleki@yahoo.com>
2024-11-04 09:27:34 +0800

    add private and public

--- Typescript_The_Complete_Guide_2023/lesson1/classes.js
+++ Typescript_The_Complete_Guide_2023/lesson1/classes.js
@@ -1,20 +1,25 @@
-var BankAccount = /** @class */ (function () {
-    function BankAccount(accountNumber, accountName, accountBalance) {
-        this.accountBalance = accountBalance;
-        this.accountName = accountName;
+"use strict";
+class BankAccount {
+    constructor(accountNumber, accountName, accountBalance) {
+        this.accountBalance = 0;
         this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.accountBalance = accountBalance;
     }
-    BankAccount.prototype.debit = function () {
+    debit() {
         console.log("debit called", this.accountNumber);
-    };
-    BankAccount.prototype.credit = function () {
+    }
+    credit() {
         console.log("credit called", this.accountNumber);
-    };
-    return BankAccount;
-}());
-var account1 = new BankAccount(101, 'Mluleki', 1500);
-var account2 = new BankAccount(102, 'Nazi', 1500);
-var account3 = new BankAccount(103, 'Phumie', 1500);
-console.log(account1.accountNumber);
+    }
+    showBalance() {
+        console.log(`Balance is ${this.accountBalance}`);
+    }
+}
+let account1 = new BankAccount(101, "Mluleki", 1500);
+let account2 = new BankAccount(102, "Nazi", 1500);
+let account3 = new BankAccount(103, "Phumie", 1500);
+let account5 = new BankAccount(104, "Muki", 0);
 account1.debit();
 account2.credit();
+account5.showBalance();
--- Typescript_The_Complete_Guide_2023/lesson1/classes.ts
+++ Typescript_The_Complete_Guide_2023/lesson1/classes.ts
@@ -1,32 +1,36 @@
 class BankAccount{
-    accountNumber:number;
-    accountName:string;
-    accountBalance:number;
+    public accountNumber:number;
+    public accountName:string;
+    private accountBalance:number = 0
 
     constructor(
         accountNumber:number,
         accountName:string,
         accountBalance:number
     ){
-        this.accountBalance= accountBalance;
+        this.accountNumber= accountNumber;
         this.accountName = accountName;
-        this.accountNumber =accountNumber;
+        this.accountBalance = accountBalance
     }
 
-    debit(){
+    public debit(){
         console.log("debit called",this.accountNumber)
     }
 
-    credit(){
+    public credit(){
         console.log("credit called",this.accountNumber)
+    }
+    public showBalance(){
+        console.log(`Balance is ${this.accountBalance}`)
     }
 }
 
-let account1 = new BankAccount(101,'Mluleki',1500)
-let account2 = new BankAccount(102,'Nazi',1500) 
-let account3 = new BankAccount(103,'Phumie',1500)
-
-console.log(account1.accountNumber)
+let account1 = new BankAccount(101,"Mluleki",1500)
+let account2 = new BankAccount(102,"Nazi",1500) 
+let account3 = new BankAccount(103,"Phumie",1500)
+let account5 = new BankAccount(104,"Muki",0)
 
 account1.debit()
 account2.credit()
+
+account5.showBalance()
