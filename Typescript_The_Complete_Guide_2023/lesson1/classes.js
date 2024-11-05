fc91e7fffa4873950d77a8ac940a13600126ddcb
mlu1 <mthandemeluleki@yahoo.com>
2024-11-05 17:07:16 +0800

    add more functionality to classes

--- Typescript_The_Complete_Guide_2023/lesson1/classes.js
+++ Typescript_The_Complete_Guide_2023/lesson1/classes.js
@@ -2,24 +2,47 @@
 class BankAccount {
     constructor(accountNumber, accountName, accountBalance) {
         this.accountBalance = 0;
+        this.debitCharges = 5;
+        this.numberOfDebits = 0;
         this.accountNumber = accountNumber;
         this.accountName = accountName;
         this.accountBalance = accountBalance;
     }
-    debit() {
-        console.log("debit called", this.accountNumber);
+    debit(amount) {
+        if (amount >= 10) {
+            this.numberOfDebits += 1;
+            console.log(`Number of Debits  + ${this.numberOfDebits}`);
+            console.log("Doing debit of", amount);
+            if (this.numberOfDebits > 3) {
+                console.log("Debit charge applied " + this.debitCharges);
+                this.accountBalance = this.accountBalance - amount - this.debitCharges;
+            }
+            this.accountBalance = this.accountBalance - amount - this.debitCharges;
+        }
+        else {
+            console.log("invalid amount");
+        }
     }
-    credit() {
-        console.log("credit called", this.accountNumber);
+    credit(amount) {
+        if (amount >= 10) {
+            console.log("Doing credit of", amount);
+            console.log("Debit charges applied", this.debitCharges);
+            this.accountBalance = this.accountBalance + amount;
+        }
+        else {
+            console.log("invalid amount");
+        }
     }
     showBalance() {
         console.log(`Balance is ${this.accountBalance}`);
     }
 }
 let account1 = new BankAccount(101, "Mluleki", 1500);
-let account2 = new BankAccount(102, "Nazi", 1500);
-let account3 = new BankAccount(103, "Phumie", 1500);
-let account5 = new BankAccount(104, "Muki", 0);
-account1.debit();
-account2.credit();
-account5.showBalance();
+account1.credit(9);
+account1.showBalance();
+account1.debit(200);
+account1.showBalance();
+account1.debit(200);
+account1.debit(200);
+account1.showBalance();
+account1.debit(200);
--- Typescript_The_Complete_Guide_2023/lesson1/classes.ts
+++ Typescript_The_Complete_Guide_2023/lesson1/classes.ts
@@ -1,7 +1,9 @@
 class BankAccount{
-    public accountNumber:number;
+    public readonly accountNumber:number;
     public accountName:string;
-    private accountBalance:number = 0
+    private accountBalance:number = 0;
+    private debitCharges:number = 5;
+    private numberOfDebits:number =0;
 
     constructor(
         accountNumber:number,
@@ -13,24 +15,44 @@ class BankAccount{
         this.accountBalance = accountBalance
     }
 
-    public debit(){
-        console.log("debit called",this.accountNumber)
+    public debit(amount:number){
+        if(amount >=10){
+            this.numberOfDebits +=1;
+            console.log(`Number of Debits  + ${this.numberOfDebits}`)
+            console.log("Doing debit of",amount)
+            if(this.numberOfDebits >3){
+                console.log("Debit charge applied " +this.debitCharges)
+                this.accountBalance = this.accountBalance - amount-this.debitCharges
+            }
+            this.accountBalance = this.accountBalance - amount-this.debitCharges
+        }else{
+            console.log("invalid amount")
+        }
+    } 
+
+    public credit(amount:number){
+        if(amount >=10){
+            console.log("Doing credit of",amount)
+            console.log("Debit charges applied", this.debitCharges)
+            this.accountBalance = this.accountBalance +amount
+        }
+        else{
+            console.log("invalid amount")
+        }
     }
 
-    public credit(){
-        console.log("credit called",this.accountNumber)
-    }
     public showBalance(){
         console.log(`Balance is ${this.accountBalance}`)
     }
 }
 
 let account1 = new BankAccount(101,"Mluleki",1500)
-let account2 = new BankAccount(102,"Nazi",1500) 
-let account3 = new BankAccount(103,"Phumie",1500)
-let account5 = new BankAccount(104,"Muki",0)
 
-account1.debit()
-account2.credit()
-
-account5.showBalance()
+account1.credit(9)
+account1.showBalance()
+account1.debit(200)
+account1.showBalance()
+account1.debit(200)
+account1.debit(200)
+account1.showBalance()
+account1.debit(200)
