fde8f27853fc43335a1d4582180477469d860124
mlu1 <mthandemeluleki@yahoo.com>
2025-01-06 18:02:57 +0800

    authorize the second context

--- project2/contextapi/src/components/Navbar.js
+++ project2/contextapi/src/components/Navbar.js
@@ -1,33 +1,40 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends React.Component {
   static contextType = ThemeContext;
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-            const { isDarkTheme, darkTheme, lightTheme } = this.context;
-            const theme = isDarkTheme ? darkTheme : lightTheme;
-            console.log(theme.background)
-          return (
-            <nav
-              style={{
-                background: theme.background,
-                color: theme.text,
-                height: "120px",
-              }}
-            >
-              <h2 style={{ textAlign: "center" }}>CubePilot Academy</h2>
-              <div className="ui three buttons">
-                <button className="ui button">Overview</button>
-                <button className="ui button">Contact</button>
-                <button className="ui button">Support</button>
-              </div>
-            </nav>
-          );
+      <AuthContext.Consumer>{()=>{
+        return(
+          <ThemeContext.Consumer>
+          {(themecontext) => {
+              const { isDarkTheme, darkTheme, lightTheme } = themecontext;
+              const theme = isDarkTheme ? darkTheme : lightTheme;
+              console.log(theme.background)
+            return (
+              <nav
+                style={{
+                  background: theme.background,
+                  color: theme.text,
+                  height: "120px",
+                }}
+              >
+                <h2 style={{ textAlign: "center" }}>CubePilot Academy</h2>
+                <div className="ui three buttons">
+                  <button className="ui button">Overview</button>
+                  <button className="ui button">Contact</button>
+                  <button className="ui button">Support</button>
+                </div>
+              </nav>
+            );
+          }}
+          </ThemeContext.Consumer>
+          )
         }}
-      </ThemeContext.Consumer>
+      </AuthContext.Consumer>
+      
     );
   }
 }
