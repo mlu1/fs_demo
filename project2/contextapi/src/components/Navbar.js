daf41d19fe92a08b2939c03a44f3b53ef53bc6cb
mlu1 <mthandemeluleki@yahoo.com>
2025-01-07 09:38:24 +0800

    consume auth and theme context in one app

--- project2/contextapi/src/components/Navbar.js
+++ project2/contextapi/src/components/Navbar.js
@@ -3,25 +3,26 @@ import { ThemeContext } from "../contexts/ThemeContext";
 import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext;
   render() {
     return (
-      <AuthContext.Consumer>{()=>{
+      <AuthContext.Consumer>{(authContext)=>{
         return(
           <ThemeContext.Consumer>
           {(themecontext) => {
               const { isDarkTheme, darkTheme, lightTheme } = themecontext;
+              const {isLoggedIn,changeAuthStatus} = authContext;
               const theme = isDarkTheme ? darkTheme : lightTheme;
               console.log(theme.background)
             return (
               <nav
-                style={{
-                  background: theme.background,
-                  color: theme.text,
-                  height: "120px",
-                }}
-              >
+                  style={{
+                    background: theme.background,
+                    color: theme.text,
+                    height: "120px",
+                  }}
+                >
                 <h2 style={{ textAlign: "center" }}>CubePilot Academy</h2>
+                <p onClick={changeAuthStatus} style = {{textAlign:'center'}}>{isLoggedIn?'logged in':'not logged in'}</p>
                 <div className="ui three buttons">
                   <button className="ui button">Overview</button>
                   <button className="ui button">Contact</button>
